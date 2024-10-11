7385884a1980b6e9997d0cc3e36e0d8a52dddf8d
le-vitalique <site.lab7@gmail.com>
2024-10-11 10:33:07 +0400

    commit

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,19 +1,25 @@
-import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_application_1.calculate()}!');
 
-  print('\n');
-
-  getData().then((value)
-  {
+  getData(3).then((value) {
     print(value);
+  }).catchError((error) {
+    print(error);
+  }).whenComplete(() {
+    print('whenComplete');
   });
+
+  //sleep(Duration(seconds: 1));
 }
 
-Future<List<String>> getData()
-{
-  return Future.delayed(Duration(seconds: 2), () {
+Future<List<String>> getData([int delay = 1]) {
+  return Future.delayed(Duration(seconds: delay), () {
+    if (delay > 2) {
+      return Future.error('timeout');
+    }
     return ['Александр', 'Мария', 'Никита'];
   });
 }
